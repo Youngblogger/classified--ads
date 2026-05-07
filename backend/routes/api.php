8fdf67013fb7047408002fc4b7c50169106b4eb7
Abdulwahab Olanrewaju <uthmanabdulwahab2019@gmail.com>
2026-05-07 03:19:42 -0600

    22

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -308,7 +308,7 @@ Route::middleware('auth.api')->group(function () {
     // Ads - protected routes (must be before /{slug} wildcard)
     Route::post('/ads', [AdController::class, 'store']);
     Route::post('/ads/boost-on-publish', [AdController::class, 'boostOnPublish']);
-    Route::get('/ads/{id}', [AdController::class, 'getById'])->where('id', '[0-9]+');
+    Route::get('/ads/{id}', [AdController::class, 'showById'])->where('id', '[0-9]+');
     Route::put('/ads/{id}', [AdController::class, 'updateById'])->where('id', '[0-9]+');
     Route::put('/ads/{slug}', [AdController::class, 'update']);
     Route::delete('/ads/{slug}', [AdController::class, 'destroy']);
